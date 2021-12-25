f7459b00dc022f585db2ebc041a7f834c2388bf9
Ian Dinwoodie <dinwoodieian@gmail.com>
2021-12-25 16:14:53 -0500

    Moved utility tests to a single file.
    Added pylint test.

---
+++ tests/misc/test_utilities.py
@@ -0,0 +1,30 @@
+# Standard library modules.
+import pathlib
+import subprocess
+
+# Third party modules.
+import pytest
+
+
+project_dir = pathlib.Path(__file__).parent.parent.parent
+
+
+def test_files_pass_black_check():
+    """Properly formatted source files should pass black."""
+    cmd = ["python", "-m", "black", "--check", project_dir]
+    p = subprocess.run(cmd, capture_output=True)
+    assert p.returncode == 0
+
+
+def test_files_pass_mypy_check():
+    """Properly structured source files should pass mypy."""
+    cmd = ["python", "-m", "mypy", "--ignore-missing-imports", project_dir]
+    p = subprocess.run(cmd, capture_output=True)
+    assert p.returncode == 0
+
+
+def test_files_pass_pylint_check():
+    """Properly structured source files should pass pylint."""
+    cmd = ["python", "-m", "pylint", project_dir / "src", project_dir / "tests"]
+    p = subprocess.run(cmd, capture_output=True)
+    assert p.returncode == 0
