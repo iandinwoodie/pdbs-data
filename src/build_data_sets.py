c418e0303896297edfc786a6580aeab16f4521f9
Ian Dinwoodie <dinwoodieian@gmail.com>
2022-01-10 23:01:45 -0500

    Verify refined data set.

--- src/build_data_sets.py
+++ src/build_data_sets.py
@@ -36,32 +36,38 @@ def main() -> None:
     print("done")
     assert df_trimmed.shape == (5057, 490)
 
-    df_demo = create_demographics_data_frame(df_trimmed)
-    assert df_demo.shape == (4150, 490)
+    df_refined = pd.read_csv(
+        config.intermediate_data_dir / "refined.csv", dtype=object, low_memory=False
+    ).apply(pd.to_numeric, errors="ignore")
+    assert df_refined.shape == (5057, 503)
 
-    df_treat = create_treatment_data_frame(df_trimmed)
-    assert df_treat.shape == (2322, 489)
+    df_demo = create_demographics_data_frame(df_refined)
+    assert df_demo.shape == (4150, 151)
+
+    df_treat = create_treatment_data_frame(df_refined)
+    assert df_treat.shape == (2322, 502)
 
 
-def create_demographics_data_frame(df_trimmed: pd.DataFrame) -> pd.DataFrame:
+def create_demographics_data_frame(df_refined: pd.DataFrame) -> pd.DataFrame:
     """
     Create a data frame containing only the demographic data.
     """
-    df_demo = df_trimmed.copy()
+    df_demo = df_refined.copy()
     # The following is the inclusion criteria.
     df_demo = df_demo.loc[
         (df_demo["question_reason_for_part_3"] == 0)
         | ((df_demo["question_reason_for_part_3"] == 1) & (df_demo["q01_main"] != 1))
     ]
-    # NOTE: All event 2 columns can be dropped for this study.
+    cols = df_demo.columns.to_series()["prof_help":"feedback_extra_2"]  # type: ignore
+    df_demo = df_demo.drop(cols, axis=1)
     return df_demo
 
 
-def create_treatment_data_frame(df_trimmed: pd.DataFrame) -> pd.DataFrame:
+def create_treatment_data_frame(df_refined: pd.DataFrame) -> pd.DataFrame:
     """
     Create a data frame containing only the treatment data.
     """
-    df_treat = df_trimmed.copy()
+    df_treat = df_refined.copy()
     # Drop incomplete responses for the second event.
     df_treat = df_treat.loc[df_treat["phase_2_complete"] == 2]
     df_treat = df_treat.drop(["phase_2_complete"], axis=1)
